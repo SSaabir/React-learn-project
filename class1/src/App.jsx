e1082b6fd6604c3f7f1a033d0986eef0a712d08d
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-12-17 21:36:13 +0530

    starting new again xd

--- class1/src/App.jsx
+++ class1/src/App.jsx
@@ -1,17 +1,7 @@
-// import React, { Fragment } from 'react'
-// import { LearnComponent } from './componets/Learncomponent'
-// import { Header } from './componets/Header'
+import React from 'react'
 
-import { Student } from "./componets/Student";
-import "./css/app.css";
-export default function App() {
+export const App = () => {
   return (
-    <>
-    {/* <Header/>
     <div>App</div>
-    <LearnComponent/> */}
-    <Student name="George" age={35} MaritalStatus={true} />
-    <Student name="Lewis" age={55} MaritalStatus={false} />
-    </>
-    )
-} 
+  )
+}
