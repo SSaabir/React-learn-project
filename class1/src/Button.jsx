b08d11681c9188118248a2ca423e2aa83d36cfd8
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-12-31 22:10:31 +0530

    learned about react styling

---
+++ class1/src/Button.jsx
@@ -0,0 +1,16 @@
+export const Button = () => {
+  const styles = {
+    backgroundColor : "blue",
+    color : "white",
+    padding : "10px 20px",
+    borderRadius : "5px",
+    border : "none",
+    cursor : "pointer",
+  }
+
+  return (
+    <button style={styles}>
+        Click Me
+    </button>
+  )
+}
