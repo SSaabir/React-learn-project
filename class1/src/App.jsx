1da4a6f74c645d1f2acdf5b1a8796c23705ced21
SSaabir <159421259+SSaabir@users.noreply.github.com>
2024-12-31 23:06:31 +0530

    props they side - it was fun after all

--- class1/src/App.jsx
+++ class1/src/App.jsx
@@ -1,9 +1,13 @@
-import { Button } from "./Button";
+import { Student } from "./Student";
 
  function App() {
   return (
  <>
- <Button/>
+    <Student name="hecrim" age={30} isStudent={true} />
+    <Student name="jeremy" age={24} isStudent={false} />
+    <Student name="aztec" age={34} isStudent={true} />
+    <Student name="nimblenumble" age={67} isStudent={false} />
+    <Student />
  </>
   )
 }
