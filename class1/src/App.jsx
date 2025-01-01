a1963f05afc1667d724c6daa9e2b62c1b9b05268
SSaabir <159421259+SSaabir@users.noreply.github.com>
2025-01-02 00:30:52 +0530

    rendering lists - a little understanding difficulty

--- class1/src/App.jsx
+++ class1/src/App.jsx
@@ -1,10 +1,21 @@
-import { UserGreeeting } from "./UserGreeeting";
+import List from "./List";
 
  function App() {
-  return (
- <>
-   <UserGreeeting isLoggedIn = {false} username = "adam" />   
- </>
+   const fruits = [
+      {id:101, name:"apple", calories: 95},
+      {id:102, name: "mango", calories: 54}, 
+      {id:103, name:"pine", calories: 37}];
+      
+      const vegitables = [
+         {id:105, name:"carrot", calories: 295},
+         {id:106, name: "celery", calories: 514}, 
+         {id:107, name:"leaks", calories: 373}];
+         
+   return (
+      <>
+   {fruits.length > 0 && <List item={fruits} category="Fruits"/>}
+   {vegitables.length > 0 && <List item={vegitables} category="Vegitables " />}
+   </>
   )
 }
 export default App;
---
+++ class1/src/List.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import propTypes from 'prop-types'
+export default function List(props) {
+
+    const category = props.category;
+    const itemList = props.item;
+
+    //fruits.sort((a,b) => a.name.localeCompare(b.name));
+    //fruits.sort((a,b) => b.name.localeCompare(a.name));
+    //fruits.sort((a,b)=> a.calories - b.calories);
+    //fruits.sort((a,b) => b.calories - a.calories);
+
+
+    //const LowCalFruits = fruits.filter(fruit => fruit.calories < 80);
+    //const HighCalFruits = fruits.filter(fruit => fruit.calories > 80);
+  const listItems = itemList.map(item => <li key={item.id}>{item.name}:&nbsp; {item.calories}</li>);  
+  
+  return (
+  <>
+  <h2>{category}</h2>
+  <ol>{listItems}</ol>
+  </>
+  )
+}
+
+List.PropTypes = {
+    category: propTypes.string,
+    item: propTypes.arrayOf(propTypes.shape({
+        id: propTypes.number,
+        name: propTypes.string,
+        calories: propTypes.number}))
+}
+
+List.defaultProps = {
+    category: "Category",
+    item: []
+}
